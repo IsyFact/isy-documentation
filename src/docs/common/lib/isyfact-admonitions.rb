8d9acfc6d89f618be82e3ce23999f8180b18d298
Bj?rn Saxe <bjoern.saxe@msg.group>
2019-03-07 15:16:38 +0100

    IFS-241: Asciidoctor-Extension refactored; Styles für HTML und PDF an richtige Stelle verschoben; Integration in Build

--- src/docs/common/lib/isyfact-admonitions.rb
+++ src/docs/common/lib/isyfact-admonitions.rb
@@ -1,7 +1,5 @@
 require 'asciidoctor/extensions'
 
-include Asciidoctor
-
 # Ein Plugin um AsciiDoctor mit Custom-Admonition-Blocks zu erweitern.
 #
 # Syntax
@@ -16,7 +14,7 @@ include Asciidoctor
 #   [ARCHITEKTURREGEL]
 #   Lorem Ipsum
 #
-class ArchitekturAdmonitionBlock < Extensions::BlockProcessor
+class ArchitekturAdmonitionBlock < Asciidoctor::Extensions::BlockProcessor
   use_dsl
   named :ARCHITEKTURREGEL
   on_contexts :example, :paragraph
@@ -29,7 +27,7 @@ class ArchitekturAdmonitionBlock < Extensions::BlockProcessor
 
 end
 
-class StyleguideAdmonitionBlock < Extensions::BlockProcessor
+class StyleguideAdmonitionBlock < Asciidoctor::Extensions::BlockProcessor
   use_dsl
   named :STYLEGUIDE
   on_contexts :example, :paragraph
@@ -42,7 +40,7 @@ class StyleguideAdmonitionBlock < Extensions::BlockProcessor
 
 end
 
-class AnwendungsanforderungAdmonitionBlock < Extensions::BlockProcessor
+class AnwendungsanforderungAdmonitionBlock < Asciidoctor::Extensions::BlockProcessor
   use_dsl
   named :ANWENDUNGSANFORDERUNG
   on_contexts :example, :paragraph
@@ -55,7 +53,7 @@ class AnwendungsanforderungAdmonitionBlock < Extensions::BlockProcessor
 
 end
 
-class SicherheitAdmonitionBlock < Extensions::BlockProcessor
+class SicherheitAdmonitionBlock < Asciidoctor::Extensions::BlockProcessor
      use_dsl
      named :SICHERHEIT
      on_contexts :example, :paragraph
@@ -68,7 +66,7 @@ class SicherheitAdmonitionBlock < Extensions::BlockProcessor
 
    end
 
-class InfoAdmonitionBlock < Extensions::BlockProcessor
+class InfoAdmonitionBlock < Asciidoctor::Extensions::BlockProcessor
   use_dsl
   named :INFO
   on_contexts :example, :paragraph
@@ -81,31 +79,12 @@ class InfoAdmonitionBlock < Extensions::BlockProcessor
 
 end
 
-class CustomAdmonitionBlock < Extensions::BlockProcessor
-  use_dsl
-  named :HILFE
-  on_contexts :example, :paragraph
-
-  def process parent, reader, attrs
-    attrs['name'] = 'hilfe'
-    attrs['caption'] = 'Hilfe'
-    create_block parent, :admonition, reader.lines, attrs, content_model: :compound
-  end
-
-end
-
-
-class CustomAdmonitionBlockDocinfo < Extensions::DocinfoProcessor
+class IsyFactAdmonitionBlockDocinfo < Asciidoctor::Extensions::DocinfoProcessor
   use_dsl
 
   def process doc
     '<style>
 .admonitionblock td.icon .icon-info:before {content:"\f05a";color:#003D86;}
-.admonitionblock td.icon .icon-note:before {content:"\f0f6";color:#003D86;}
-.admonitionblock td.icon .icon-tip:before {content:"\f0eb";color:#003D86;}
-.admonitionblock td.icon .icon-important:before {content:"\f0a1";color:#003D86;}
-.admonitionblock td.icon .icon-warning:before {content:"\f071";color:#003D86;}
-.admonitionblock td.icon .icon-caution:before {content:"\f06a";color:#003D86;}
 .admonitionblock td.icon .icon-architekturregel:before {content:"\f19c";color:#003D86;}
 .admonitionblock td.icon .icon-sicherheit:before {content:"\f132";color:#003D86;}
 .admonitionblock td.icon .icon-styleguide:before {content:"\f1fc";color:#003D86;}
@@ -114,3 +93,11 @@ class CustomAdmonitionBlockDocinfo < Extensions::DocinfoProcessor
   end
 end
 
+Asciidoctor::Extensions.register do
+  block ArchitekturAdmonitionBlock
+  block StyleguideAdmonitionBlock
+  block AnwendungsanforderungAdmonitionBlock
+  block SicherheitAdmonitionBlock
+  block InfoAdmonitionBlock
+  docinfo_processor IsyFactAdmonitionBlockDocinfo
+end
